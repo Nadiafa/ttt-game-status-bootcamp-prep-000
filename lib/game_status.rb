fe81d071fe2e54f059ff6aa270d5bf443733d1ee
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-07 13:21:26 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -34,7 +34,7 @@ def draw?(board)
 end   
 
 def over?(board)
-  full?(board) &&won?(board)
+  full?(board) && won?(board)
   #   returns true for a won game when the board is full
   #   returns true for a won game when the board is not full
   #   returns false for an in-progress game
