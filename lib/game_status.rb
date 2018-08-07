671a259a3b8a0089d75f4b6bcb641d795e33fbec
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-07 12:55:58 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,22 +5,22 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0,1,2], 
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
+  [0,1,2],  #Top Row 
+  [3,4,5],  #Middle Row 
+  [6,7,8],  #Bottom Row 
+  [0,3,6],  #Left Column
+  [1,4,7],  #Middle Column
+  [2,5,8],  #Right Column
+  [0,4,8],  #Left Diagonal
+  [2,4,6]   #Right Diagonal
 ]
 
-#won? returns false for an empty board
 def won?(board)
-    board.include?("X")
-end
-def won?(board)
-    board.include?("O")
+    #won? returns false for an empty board
+    board.include?("X") || board.include?("O")
+    
+    
+    
 end
 
 #full? #draw? #over? #winner
