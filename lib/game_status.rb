fef31e70863945dbeead0bdcff8604bab73ce771
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-07 13:16:08 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,7 +26,7 @@ end
 
    
 def full?(board)
-  board.all? {|move| move == "" || move == " "}
+  board.all? {|move| move == "X" || move == "O"}
   #   returns true for a draw
   #   returns false for an in-progress game
   
