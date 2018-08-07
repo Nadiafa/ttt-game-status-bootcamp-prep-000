87e7d33dfdb4cf188f25fa6736248865b42d3c51
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-07 13:32:34 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,13 +16,23 @@ WIN_COMBINATIONS = [
   [2,4,6]   #Right Diagonal
 ]
 
+# def won?(board)
+#   WIN_COMBINATIONS.detect do |win_combo|
+#     board[win_combo[0]] == board[win_combo[1]] &&
+#     board[win_combo[1]] == board[win_combo[2]] &&
+#     position_taken?(board, win_combo[0])
+#   end
+# end
+
 def won?(board)
-  WIN_COMBINATIONS.detect do |win_combo|
-    board[win_combo[0]] == board[win_combo[1]] &&
-    board[win_combo[1]] == board[win_combo[2]] &&
-    position_taken?(board, win_combo[0])
-  end
+  WIN_COMBINATIONS.detect {|win_combo| board[win_combo[0]] == board[win_combo[1]] && board[win_combo[1]] == board[win_combo[2]] &&
+    position_taken?(board, win_combo[0])}
 end
+
+
+
+
+
 
 def full?(board)
   board.all? {|move| move == "X" || move == "O"}
